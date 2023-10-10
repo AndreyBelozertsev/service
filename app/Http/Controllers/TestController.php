8324238d98b549932fed8a4a130c233cf97b4134
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2023-10-10 16:56:32 +0300

    TestApi

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Log;
 class TestController extends Controller
 {
     public function index(Request $request){
-        Log::info($request);
-        return response('https://123.ru');   
+ 
     }
 }
