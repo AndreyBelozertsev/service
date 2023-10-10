39145039c3fb5c5b6ed0b9a17c096aeeb78419dd
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2023-10-10 16:53:38 +0300

    TestApi

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/test', [ProfileFeedbackController::class, 'index'])->name('test');
-
-
 require __DIR__.'/auth.php';
