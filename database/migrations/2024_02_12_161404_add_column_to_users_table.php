34c8c24ada6a9e972b3cd4fc91b4a0f4035413dd
AndreyBelozertsev <andrey.belozertsev@gmail.com>
2024-02-13 11:47:33 +0300

    feat:AddRolePermission

--- database/migrations/2024_02_12_161404_add_column_to_users_table.php
+++ database/migrations/2024_02_12_161404_add_column_to_users_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role')->default('manager')->after('name');
             $table->boolean('status')->default(1)->after('password');
         });
     }
@@ -23,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 };
